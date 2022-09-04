8d4d6d29e1fe291ba840f807d893c13251f7660a
Bharadwaj Palakurthy <bharadwajpalakurthy@gmail.com>
2022-09-04 20:03:27 +0530

    test walletConnect

--- src/global/components/cantoNav.tsx
+++ src/global/components/cantoNav.tsx
@@ -8,11 +8,12 @@ import {
 import { useEffect } from "react";
 import { useNetworkInfo } from "pages/main/hooks/networkInfo";
 import logo from "./../../assets/logo.svg";
+import WalletConnectProvider from "@walletconnect/web3-provider/";
 
 export const CantoNav = () => {
   const netWorkInfo = useNetworkInfo();
   const alert = useAlert();
-  const { activateBrowserWallet, account } = useEthers();
+  const { activateBrowserWallet, account, activate } = useEthers();
 
   async function setChainInfo() {
     const [chainId, account] = await getChainIdandAccount();
@@ -70,6 +71,17 @@ export const CantoNav = () => {
     getBalance();
   }, [netWorkInfo.account]);
 
+  async function onConnect() {
+    try {
+      const provider = new WalletConnectProvider({
+        infuraId: "099be1bf60eab5b2a67865aa081cf4ad",
+      });
+      await provider.enable();
+      await activate(provider);
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
     <NavBar
       pageList={[
@@ -79,7 +91,8 @@ export const CantoNav = () => {
         },
       ]}
       onClick={() => {
-        activateBrowserWallet();
+        // activateBrowserWallet();
+        onConnect();
         addNetwork();
       }}
       chainId={Number(netWorkInfo.chainId)}
